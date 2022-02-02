b9c7fc907b5f3dd203148c0f071f26d9d6a9eeaa
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-02-02 22:36:21 +0000

    Add counts to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def show
+    @products_count = Product.count()
+    @categories_count = Category.count()
   end
+  
 end
